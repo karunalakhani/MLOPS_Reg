a424cb90150bf81ea0920870232f9e5a892bc01f
Karuna <karuna25@live.com>
2024-01-16 17:41:37 +0530

    Initial Project

---
+++ template.py
@@ -0,0 +1,45 @@
+import os
+
+dirs = [
+    os.path.join("data", "raw"),
+    os.path.join("data","processed"),
+    "data_given",
+    "notebooks",
+    "saved_models",
+    "src",
+    "report",
+    "tests",
+    "artifacts",
+    os.path.join("prediction_service", "model"),
+    os.path.join("webapp", "static", "css"),
+    os.path.join("webapp", "static", "script"),
+    os.path.join("webapp", "templates")
+
+]
+
+for dir in dirs:
+    os.makedirs(dir, exist_ok=True)
+    #Create .gitkeep file for commiting empty folder to git
+    with open(os.path.join(dir, ".gitkeep"), "w") as f:
+        pass
+
+files = [
+    "dvc.yaml",
+    "params.yaml",
+    ".gitignore",
+    "app.py",
+    os.path.join("src","__init__.py"),
+    os.path.join("prediction_service", "prediction.py"),
+    os.path.join("webapp", "static", "css", "main.css"),
+    os.path.join("webapp", "static", "css", "index.js"),
+    os.path.join("webapp", "templates","index.html"),
+    os.path.join("webapp", "templates","404.html"),
+    os.path.join("webapp", "templates","base.html"),
+    os.path.join(".github", "workflows","ci-cd.yaml"),
+    os.path.join("src", "log_production_model.py")
+
+]
+
+for file in files:
+    with open(file, "w") as f:
+        pass
